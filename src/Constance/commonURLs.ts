b37bbda3f2e3e6867754f93e1d42085393b1d127
Samarasimha Reddy <samarasimha.reddy@innovatechs.com>
2026-07-02 17:54:01 +0530

    chore: change backend local port from 5000 to 5050 to avoid macOS EADDRINUSE conflict

--- src/Constance/commonURLs.ts
+++ src/Constance/commonURLs.ts
@@ -1,7 +1,7 @@
 import {API_URL} from '@env';
 
 export const ENV = {
-  API_URL: API_URL || 'http://localhost:5000',
+  API_URL: API_URL || 'http://localhost:5050',
   API_TIMEOUT: 15000,
   IS_DEV: __DEV__,
   MOCK_REFRESH_TOKEN_EXPIRY: 60 * 1000,
