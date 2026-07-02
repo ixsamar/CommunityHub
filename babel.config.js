2de6c5aac4b28a5326b711159cbc93d4fd8a1fac
Samarasimha Reddy <samarasimha.reddy@innovatechs.com>
2026-07-02 17:59:35 +0530

    chore: remove react-native-dotenv and use static local env file resolver

--- babel.config.js
+++ babel.config.js
@@ -17,17 +17,6 @@ module.exports = {
         },
       },
     ],
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
     'react-native-reanimated/plugin',
   ],
 };
--- src/Constance/commonURLs.ts
+++ src/Constance/commonURLs.ts
@@ -1,7 +1,8 @@
-import {API_URL} from '@env';
+import {API_URL as devUrl} from './env';
+import {API_URL as prodUrl} from './env.production';
 
 export const ENV = {
-  API_URL: API_URL || 'http://localhost:5050',
+  API_URL: __DEV__ ? devUrl : prodUrl,
   API_TIMEOUT: 15000,
   IS_DEV: __DEV__,
   MOCK_REFRESH_TOKEN_EXPIRY: 60 * 1000,
